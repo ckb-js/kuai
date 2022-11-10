0e9b0b620c21b2cdd94569cfb985ad8acce165bd
painterpuppets <painterpuppets@gmail.com>
2022-11-10 08:44:15 +0800

    chore: remove cli pkg test

--- packages/cli/__tests__/index.ts
+++ packages/cli/__tests__/index.ts
@@ -1,12 +1,5 @@
-import { describe, expect, test } from '@jest/globals';
-import { execSync } from 'node:child_process';
+import { describe, test } from '@jest/globals';
 
 describe('kuai cli', () => {
-  test('ckb listening port', () => {
-    const output1 = execSync('yarn kuai node');
-    expect(output1.toString()).toMatch(/ckb running on:\s+8114/);
-
-    const output2 = execSync('yarn kuai node --port 9999');
-    expect(output2.toString()).toMatch(/ckb running on:\s+9999/);
-  });
+  test.todo('ckb listening port');
 });
