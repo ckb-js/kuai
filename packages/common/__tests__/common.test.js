008b7e5e326f4c283e9e0207a2595d3d9e5c0d0d
Keith <keithwhisper@gmail.com>
2022-11-21 11:53:21 +0800

    refactor: remove unused @kuai/common

--- packages/common/__tests__/common.test.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-console.info('common tests passed');
--- packages/common/src/utils.ts
+++
@@ -1,4 +0,0 @@
-export const isUndefined = (v: unknown): v is undefined => typeof v === 'undefined';
-export const isNil = (v: unknown): v is null | undefined => isUndefined(v) || v === null;
-export const isSymbol = (v: unknown): v is symbol => typeof v === 'symbol';
-export const isString = (v: unknown): v is string => typeof v === 'string';
