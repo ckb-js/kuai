fe3d8ae196aaef67dcc521a9722ce8b17d4676a5
painterpuppets <painterpuppets@gmail.com>
2022-11-24 04:32:09 +0800

    feat: implement kuai context

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,7 @@ export { paramTypes } from './params';
 export * from './type';
 export * from './errors';
 export * from './errors-list';
+export * from './config';
+export { KuaiContext } from './context';
+export * from './typescript-support';
+export * from './helper';
---
+++ packages/core/src/project-structure.ts
@@ -0,0 +1,19 @@
+import findUp from 'find-up';
+
+const JS_CONFIG_FILENAME = 'kuai.config.js';
+const TS_CONFIG_FILENAME = 'kuai.config.ts';
+
+export function isCwdInsideProject(): boolean {
+  return findUp.sync(TS_CONFIG_FILENAME) !== null || findUp.sync(JS_CONFIG_FILENAME) !== null;
+}
+
+export function getUserConfigPath(): string | undefined {
+  const tsConfigPath = findUp.sync(TS_CONFIG_FILENAME);
+  if (tsConfigPath) {
+    return tsConfigPath;
+  }
+
+  const pathToConfigFile = findUp.sync(JS_CONFIG_FILENAME);
+
+  return pathToConfigFile;
+}
