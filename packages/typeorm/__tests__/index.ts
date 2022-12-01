a983ffc01fabf11a17f14f467653252beeeb7580
felicityin <yinjingping2022@gmail.com>
2022-12-01 14:12:50 +0800

    chore: eliminate incorrect error prompts

--- packages/typeorm/__tests__/index.ts
+++ packages/typeorm/__tests__/index.ts
@@ -3,30 +3,27 @@ import { getMetadataArgsStorage } from 'typeorm'
 
 import { Service, KuaiEntity, Location, container } from '../src'
 
-@Service()
 class DummyService {}
-
-@Service('Named')
 class NamedService {}
-
-@KuaiEntity(Location.Data)
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class DummyEntity {}
-
-@KuaiEntity(Location.Witness, 'Named')
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class NamedEntity {}
 
 describe(`decorator`, () => {
   describe(`service decorator`, () => {
     it(`should get a right service from the container`, () => {
+      Service()(DummyService)
+      Service('Named')(NamedService)
+
       expect(container.get<DummyService>(DummyService.name)).toBeInstanceOf(DummyService)
       expect(container.get<NamedService>('Named')).toBeInstanceOf(NamedService)
     })
   })
 
   describe(`kuai entity decorator`, () => {
-    it(`should get a right service from the container`, () => {
+    it(`should find a right entity from the typerom metadata storage`, () => {
+      KuaiEntity(Location.Data)(DummyEntity)
+      KuaiEntity(Location.Witness, 'Named')(NamedEntity)
+
       let findDummyEntity = false
       let findNamedEntity = false
 
