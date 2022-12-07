83980c88076a075a00199807495c76159fd2e7cf
yanguoyu <841185308@qq.com>
2022-12-07 23:04:53 +0800

    fix: Add some exception types for json storage. Use import type.

--- packages/models/src/exceptions/store.ts
+++ packages/models/src/exceptions/store.ts
@@ -1,11 +1,29 @@
 export class NonExistentException extends Error {
   constructor(path: string) {
-    super(`The ${path} is not exist in state`)
+    super(`The ${path} does not exist in state`)
   }
 }
 
 export class NonStorageInstanceException extends Error {
   constructor() {
     super(`Storage instance should be initialized`)
+  }
+}
+
+export class UnexpectedTypeException extends Error {
+  constructor(type: string) {
+    super(`Unexpected type: ${type} in storage`)
+  }
+}
+
+export class UnexpectedMarkException extends Error {
+  constructor(mark: string) {
+    super(`Unexpected mark: ${mark} in storage`)
+  }
+}
+
+export class NoExpectedDataException extends Error {
+  constructor() {
+    super(`Storage does not include data or witness`)
   }
 }
