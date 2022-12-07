c6b304e1275b88e0811f9fe716588697e52d4f7b
yanguoyu <841185308@qq.com>
2022-12-07 23:08:37 +0800

    fix: Fix test case.

--- packages/models/__tests__/store/json-storage.ts
+++ packages/models/__tests__/store/json-storage.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from '@jest/globals'
-import { JSONStorage } from '../../src/store'
+import { JSONStorage } from '../../src'
 
 describe('test json storage', () => {
   describe('only data exist', () => {
@@ -11,21 +11,23 @@ describe('test json storage', () => {
     })
 
     it('only one layer', () => {
-      const storage = new JSONStorage<{ data: { a: number; b: string; c: boolean; d: boolean; e: bigint }}>()
-      const data = { a: 1, b: 'b', c: true, d: false, e: BigInt('100')}
+      const storage = new JSONStorage<{ data: { a: number; b: string; c: boolean; d: boolean; e: bigint } }>()
+      const data = { a: 1, b: 'b', c: true, d: false, e: BigInt('100') }
       const res = storage.serialize({ data })
       const original = storage.deserialize(res)
       Object.keys(data).forEach((key) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         expect((original.data as any)[key]).toEqual((data as any)[key])
       })
     })
 
     it('more than one layer', () => {
-      const storage = new JSONStorage<{ data: { lay: {a: number; b: string; c: boolean; d: boolean; e: bigint }}}>()
-      const data = { a: 1, b: 'b', c: true, d: false, e: BigInt('100')}
+      const storage = new JSONStorage<{ data: { lay: { a: number; b: string; c: boolean; d: boolean; e: bigint } } }>()
+      const data = { a: 1, b: 'b', c: true, d: false, e: BigInt('100') }
       const res = storage.serialize({ data: { lay: data } })
       const original = storage.deserialize(res)
       Object.keys(data).forEach((key) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         expect((original.data.lay as any)[key]).toEqual((data as any)[key])
       })
     })
@@ -39,21 +41,25 @@ describe('test json storage', () => {
       expect(original).toStrictEqual({ witness: {} })
     })
     it('only one layer', () => {
-      const storage = new JSONStorage<{ witness: { a: number; b: string; c: boolean; d: boolean; e: bigint }}>()
-      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100')}
+      const storage = new JSONStorage<{ witness: { a: number; b: string; c: boolean; d: boolean; e: bigint } }>()
+      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100') }
       const res = storage.serialize({ witness })
       const original = storage.deserialize(res)
       Object.keys(witness).forEach((key) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         expect((original.witness as any)[key]).toEqual((witness as any)[key])
       })
     })
 
     it('more than one layer', () => {
-      const storage = new JSONStorage<{ witness: { lay: {a: number; b: string; c: boolean; d: boolean; e: bigint }}}>()
-      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100')}
+      const storage = new JSONStorage<{
+        witness: { lay: { a: number; b: string; c: boolean; d: boolean; e: bigint } }
+      }>()
+      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100') }
       const res = storage.serialize({ witness: { lay: witness } })
       const original = storage.deserialize(res)
       Object.keys(witness).forEach((key) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         expect((original.witness.lay as any)[key]).toEqual((witness as any)[key])
       })
     })
@@ -68,11 +74,15 @@ describe('test json storage', () => {
     })
 
     it('more than one layer', () => {
-      const storage = new JSONStorage<{ data: number, witness: { lay: {a: number; b: string; c: boolean; d: boolean; e: bigint }}}>()
-      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100')}
+      const storage = new JSONStorage<{
+        data: number
+        witness: { lay: { a: number; b: string; c: boolean; d: boolean; e: bigint } }
+      }>()
+      const witness = { a: 1, b: 'b', c: true, d: false, e: BigInt('100') }
       const res = storage.serialize({ data: 10, witness: { lay: witness } })
       const original = storage.deserialize(res)
       Object.keys(witness).forEach((key) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         expect((original.witness.lay as any)[key]).toEqual((witness as any)[key])
       })
       expect(original.data).toBe(10)
