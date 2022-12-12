05e73b76b3caa410a462492b0c163e3a6d783f95
yanguoyu <841185308@qq.com>
2022-12-12 15:19:50 +0800

    fix: Use better name for type

--- packages/models/src/store/chain-storage.ts
+++ packages/models/src/store/chain-storage.ts
@@ -1,7 +1,7 @@
-type OneOfStorageLoc<Data, Witness = Data> = { data: Data; witness?: Witness } | { data?: Data; witness: Witness }
+type AtLeastOneOfLoc<Data, Witness = Data> = { data: Data; witness?: Witness } | { data?: Data; witness: Witness }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type StorageOffChain<Data = any, Witness = Data> = OneOfStorageLoc<Data, Witness>
-export type StorageOnChain = OneOfStorageLoc<Uint8Array>
+export type StorageOffChain<Data = any, Witness = Data> = AtLeastOneOfLoc<Data, Witness>
+export type StorageOnChain = AtLeastOneOfLoc<Uint8Array>
 
 export abstract class ChainStorage<Data extends StorageOffChain = StorageOffChain> {
   abstract serialize(data: Data): StorageOnChain
