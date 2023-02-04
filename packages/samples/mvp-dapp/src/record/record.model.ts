424d82a812856ae2ae565452578dc028a53ce98a
yanguoyu <841185308@qq.com>
2023-02-04 11:18:47 +0800

    feat: Define Record model

---
+++ packages/samples/mvp-dapp/src/record/record.model.ts
@@ -0,0 +1,32 @@
+import { JSONStore, OutPointString, UpdateStorageValue } from '@ckb-js/kuai-models'
+
+type ItemData = {
+  key: string
+  value: string
+  label: string
+}
+
+export type StoreType = {
+  data: {
+    profile: ItemData[]
+    addresses: ItemData[]
+    custom: ItemData[]
+    dweb: ItemData[]
+    permissions: ItemData[]
+  }
+}
+
+/**
+ * add business logic in an actor
+ */
+export class RecordModel extends JSONStore<StoreType> {
+  constructor(
+    _schemaOption?: { data: true },
+    params?: {
+      states?: Record<OutPointString, StoreType>
+      chainData?: Record<OutPointString, UpdateStorageValue>
+    },
+  ) {
+    super({ data: true }, params)
+  }
+}
