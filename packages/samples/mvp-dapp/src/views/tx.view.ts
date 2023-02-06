cdb1aad571e0b66fe51ebd850896d4b5016b3a97
daryl <sai9375@hotmail.com>
2023-02-06 12:48:34 +0800

    feat(sample): support omni lock

--- packages/samples/mvp-dapp/src/views/tx.view.ts
+++ packages/samples/mvp-dapp/src/views/tx.view.ts
@@ -1,20 +1,34 @@
 import { CellChangeData } from '@ckb-js/kuai-models'
-import { Cell, helpers, commons } from '@ckb-lumos/lumos'
+import { Cell, helpers, commons, config } from '@ckb-lumos/lumos'
+import { SECP_SIGNATURE_PLACEHOLDER, OMNILOCK_SIGNATURE_PLACEHOLDER } from '@ckb-lumos/common-scripts/lib/helper'
+import { blockchain } from '@ckb-lumos/base'
+import { bytes } from '@ckb-lumos/codec'
 
 export class Tx {
   static async toJson(
     txSkeleton: helpers.TransactionSkeletonType,
-    inputCells: Cell[],
-    outputData: CellChangeData[],
+    inputData: CellChangeData[],
+    outputCells: Cell[],
   ): Promise<string> {
-    txSkeleton.update('inputs', (inputs) => {
-      return inputs.push(...inputCells)
+    txSkeleton.update('outputs', (outputs) => {
+      return outputs.push(...outputCells)
     })
 
-    for (const [output, witness] of outputData) {
-      txSkeleton.update('outputs', (outputs) => outputs.push(output))
+    for (const [input, witness] of inputData) {
+      txSkeleton.update('inputs', (inputs) => inputs.push(input))
 
-      txSkeleton.update('witnesses', (witnesses) => witnesses.push(witness))
+      txSkeleton.update('witnesses', (witnesses) => {
+        if (witness == '0x' || witness.length == 0) {
+          const omniLock = config.getConfig().SCRIPTS.OMNI_LOCK as NonNullable<config.ScriptConfig>
+          const fromLockScript = input.cellOutput.lock
+          if (omniLock.CODE_HASH === fromLockScript.codeHash && fromLockScript.hashType === omniLock.HASH_TYPE) {
+            return witnesses.push(bytes.hexify(blockchain.WitnessArgs.pack({ lock: OMNILOCK_SIGNATURE_PLACEHOLDER })))
+          } else {
+            return witnesses.push(bytes.hexify(blockchain.WitnessArgs.pack({ lock: SECP_SIGNATURE_PLACEHOLDER })))
+          }
+        }
+        return witnesses.push(witness)
+      })
     }
 
     txSkeleton = commons.common.prepareSigningEntries(txSkeleton)
