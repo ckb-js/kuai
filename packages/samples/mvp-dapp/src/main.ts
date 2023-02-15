627530b06cae4cd43e074e4d4c6719e8a4c6db62
painterpuppets <painterpuppets@gmail.com>
2023-02-15 14:48:23 +0800

    chore: mvp dapp add lumos config

--- packages/samples/mvp-dapp/src/main.ts
+++ packages/samples/mvp-dapp/src/main.ts
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import { koaBody } from 'koa-body'
 import { initialKuai } from '@ckb-js/kuai-core'
+import { config } from '@ckb-lumos/lumos'
 import { KoaRouterAdapter, CoR, TipHeaderListener } from '@ckb-js/kuai-io'
 import { router } from './app.controller'
 import './type-extends'
@@ -10,6 +11,23 @@ import { NervosChainSource } from './chain-source'
 async function bootstrap() {
   const kuaiCtx = await initialKuai()
   const kuaiEnv = kuaiCtx.getRuntimeEnvironment()
+  const lumosConfig: config.Config = (() => {
+    if (kuaiEnv.config.lumosConfig === 'aggron4') {
+      return config.predefined.AGGRON4
+    }
+
+    if (kuaiEnv.config.lumosConfig === 'lina') {
+      return config.predefined.LINA
+    }
+
+    if (kuaiEnv.config.lumosConfig) {
+      return kuaiEnv.config.lumosConfig
+    }
+
+    return { PREFIX: 'ckt', SCRIPTS: {} }
+  })()
+
+  config.initializeConfig(lumosConfig)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const port = (kuaiEnv.config as any)?.port || 3000
 
