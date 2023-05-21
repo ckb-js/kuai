3ee7e0860bf4b1899976d5fb3c73f565c22da0a8
yanguoyu <841185308@qq.com>
2023-05-21 23:14:24 +0800

    fix: Ignore comm mock for test

--- packages/models/jest.config.js
+++ packages/models/jest.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['./__tests__/comm-mock.ts'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json',
     },
   },
-};
+}
