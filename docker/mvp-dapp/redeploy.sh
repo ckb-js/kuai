ca79c6d66455252b15b82111d508bac7b4f5a32e
painterpuppets <painterpuppets@gmail.com>
2023-06-21 12:53:48 +0800

    chore: update webhook

--- docker/mvp-dapp/redeploy.sh
+++ docker/mvp-dapp/redeploy.sh
@@ -1,10 +1,3 @@
 #!bin/bash
-
-ORG="${DOCKER_ORG:-ckbjs}"
-IMAGE="${MVP_DAPP_IMAGE:-kuai-mvp-dapp}"
-PORT="${MVP_DAPP_PORT:-80}"
-
-docker pull $ORG/$IMAGE:latest
-docker stop $(docker ps -q --filter ancestor=$ORG/$IMAGE)
-docker system prune -f
-docker run --network host -e PORT=$PORT -e HOST=0.0.0.0 -d --restart=always $ORG/$IMAGE
+docker compose -f ./docker-compose.yml pull
+docker compose -f ./docker-compose.yml up --force-recreate -d
