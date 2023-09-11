db96145e92f8b65f1f1bf8dc3be6e1b762f22d5b
daryl <sai9375@hotmail.com>
2023-09-11 17:29:53 +0800

    feat(contract-manager): validation before writing to config files.

--- packages/common/src/contract/kuai.loader.ts
+++ packages/common/src/contract/kuai.loader.ts
@@ -1,5 +1,6 @@
 import type { ContractDeploymentInfo, ContractLoader } from './interface'
 import fs from 'node:fs'
+import { validateName, validatePath, validateScriptBase, validatecellDep } from './validator'
 
 export class KuaiContractLoader implements ContractLoader {
   constructor(private path: string) {}
@@ -7,5 +8,14 @@ export class KuaiContractLoader implements ContractLoader {
   load = (): ContractDeploymentInfo[] =>
     fs.existsSync(this.path) ? JSON.parse(fs.readFileSync(this.path, 'utf-8')) ?? [] : []
 
-  write = (info: ContractDeploymentInfo[]) => fs.writeFileSync(this.path, Buffer.from(JSON.stringify(info)))
+  write = (info: ContractDeploymentInfo[]) => {
+    if (!Array.isArray(info)) {
+      throw new Error(`info must be an array`)
+    }
+    for (const item of info) {
+      validatecellDep(validatePath(validateName(validateScriptBase(validatecellDep(item)))))
+    }
+
+    fs.writeFileSync(this.path, Buffer.from(JSON.stringify(info)))
+  }
 }
---
+++ packages/common/src/contract/validator.ts
@@ -0,0 +1,86 @@
+import { ContractDeploymentInfo } from './interface'
+
+export const validateCellDeps = (info: ContractDeploymentInfo) => {
+  if (info.cellDeps) {
+    if (!Array.isArray(info.cellDeps)) {
+      throw new Error(`cellDeps must be an array`)
+    }
+
+    if (
+      !info.cellDeps.every((cellDep) => {
+        if ('name' in cellDep) {
+          return true
+        } else if (!('cellDep' in cellDep)) {
+          false
+        } else {
+          if (
+            'outPoint' in cellDep.cellDep &&
+            'txHash' in cellDep.cellDep.outPoint &&
+            'index' in cellDep.cellDep.outPoint &&
+            ['code', 'depGroup'].includes(cellDep.cellDep.depType)
+          ) {
+            return true
+          } else {
+            return false
+          }
+        }
+      })
+    ) {
+      throw new Error(`cellDeps must be an array of { name: string } or { cellDep: CellDep }`)
+    }
+  }
+
+  return info
+}
+
+export const validateScriptBase = (info: ContractDeploymentInfo) => {
+  if (!info.scriptBase) {
+    throw new Error(`scriptBase is required`)
+  }
+
+  if (!('codeHash' in info.scriptBase)) {
+    throw new Error(`codeHash is required`)
+  }
+
+  if (!('hashType' in info.scriptBase)) {
+    throw new Error(`hashType is required`)
+  }
+
+  return info
+}
+
+export const validateName = (info: ContractDeploymentInfo) => {
+  if (!info.name) {
+    throw new Error(`name is required`)
+  }
+
+  return info
+}
+
+export const validatePath = (info: ContractDeploymentInfo) => {
+  if (!info.path) {
+    throw new Error(`path is required`)
+  }
+
+  return info
+}
+
+export const validatecellDep = (info: ContractDeploymentInfo) => {
+  if (!('outPoint' in info)) {
+    throw new Error(`outPoint is required`)
+  }
+
+  if (!('txHash' in info.outPoint)) {
+    throw new Error(`txHash is required`)
+  }
+
+  if (!('index' in info.outPoint)) {
+    throw new Error(`index is required`)
+  }
+
+  if (!info.depType || !['code', 'depGroup'].includes(info.depType)) {
+    throw new Error(`depType must be code or depGroup`)
+  }
+
+  return info
+}
