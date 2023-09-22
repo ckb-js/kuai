766f0bd26df86c31e9df285cc592bf37282b3297
yanguoyu <841185308@qq.com>
2023-09-22 09:31:13 +0800

    fix: Support send and destory from some lock scripts.

--- packages/samples/sudt/src/actors/index.ts
+++ packages/samples/sudt/src/actors/index.ts
@@ -1,8 +1,8 @@
-import { Registry } from '@ckb-js/kuai-models';
+import { Registry } from '@ckb-js/kuai-models'
 
-const appRegistry = new Registry();
-appRegistry.load(__dirname);
+const appRegistry = new Registry()
+appRegistry.load(__dirname)
 
-export { appRegistry };
-export { OmnilockModel } from './omnilock.model';
-export { SudtModel } from './sudt.model';
+export { appRegistry }
+export { OmnilockModel } from './omnilock.model'
+export { SudtModel } from './sudt.model'
